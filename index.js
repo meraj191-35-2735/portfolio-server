e23d3598c801ac4dfadaf261eb3dfa198cf67107
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-06-10 00:28:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.download("./resume.pdf");
+});
+
+app.listen(port, () => {
+  console.log(`My Portfolio Server is Run On ${port}`);
+});
