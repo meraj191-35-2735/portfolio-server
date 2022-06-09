1c8515695b30058362198402b5c080cc86ca29ed
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-06-10 00:59:36 +0600

    some changed

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+
 
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Server is running");
-  res.download("./resume.pdf");
+  res.download("./merajResume.pdf");
 });
 
 app.listen(port, () => {
